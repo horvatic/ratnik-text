21076d4a5c96e51d3cf65d6c694d534460eb0e7f
horvatic <shorvatic@gmail.com>
2020-02-25 23:18:28 -0500

    [WIP]

---
+++ ratnik-text/ratnik-text-console/InputReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ratnik_text_console
+{
+    public class InputReader
+    {
+        public void Start()
+        {
+            var capacity = 1028;
+            var buffer = new char[capacity];
+            var pos = 0;
+            while(true)
+            {
+                var c = Console.ReadKey(true);
+                if (c.Key == ConsoleKey.Backspace)
+                {
+                    if (pos != 0)
+                    {
+                        pos -= 1;
+                        buffer[pos] = ' ';
+                        Print(pos, buffer);
+                    }
+                } 
+                else if(c.Key == ConsoleKey.Enter)
+                {
+                    buffer[pos] = '\n';
+                    Print(pos, buffer);
+                    pos++;
+                }
+                else
+                {
+                    buffer[pos] = c.KeyChar;
+                    Print(pos, buffer);
+                    pos++;
+                    if(pos > capacity)
+                    {
+                        capacity *= 2;
+                        Array.Resize(ref buffer, capacity);
+                    }
+                }
+            }
+        }
+
+        private void Print(int pos, char[] buffer)
+        {
+            Console.SetCursorPosition(pos, 0);
+            Console.Write(buffer[pos]);
+        }
+    }
+}
---
+++ ratnik-text/ratnik-text-console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ratnik_text_console
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var readerTask = new TaskFactory().StartNew(() => new InputReader().Start());
+            await readerTask;
+        }
+    }
+}
